bb5c25a42165972a4b429738d2e3d72a53416bbf
devbotbo <7118.eth@proton.me>
2025-11-21 23:03:24 -0300

    Add Hardhat configuration for Oasis Sapphire, update dependencies, and create .gitignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,35 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import "@oasisprotocol/sapphire-hardhat";
 import "@nomicfoundation/hardhat-toolbox-viem";
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {
+  mnemonic: "test test test test test test test test test test test junk",
+  path: "m/44'/60'/0'/0",
+  initialIndex: 0,
+  count: 20,
+  passphrase: "",
+};
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sapphire: {
+      url: "https://sapphire.oasis.io",
+      chainId: 0x5afe,
+      accounts,
+    },
+    "sapphire-testnet": {
+      url: "https://testnet.sapphire.oasis.io",
+      accounts,
+      chainId: 0x5aff,
+    },
+    "sapphire-localnet": {
+      // docker run -it -p8544-8548:8544-8548 ghcr.io/oasisprotocol/sapphire-localnet
+      url: "http://localhost:8545",
+      chainId: 0x5afd,
+      accounts,
+    },
+  },
 };
 
 export default config;
